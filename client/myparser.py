a08073268dbd33943e272945721bfb6022101728
VooDooBlooD <mdan.hagh@gmail.com>
2015-12-30 14:16:57 +0330

    Send none move in parser

--- client/myparser.py
+++ client/myparser.py
@@ -8,9 +8,14 @@ class Parser:
     @staticmethod
     def decode(data):
         data = unpack('HB', data)
+        if data[1] == 255:
+            return data[0], None
         return data[0], Move(data[1] % 8, data[1] // 8)
 
     @staticmethod
     def encode(turn, move):
-        p = move.y * 8 + move.x
+        if move == None:
+            p = 255
+        else:
+            p = move.y * 8 + move.x
         return pack('HB', turn, p)
